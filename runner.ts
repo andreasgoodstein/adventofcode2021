713e58fa0ded603acfacd923efdfd1c24eb84787
Andreas Goodstein <24245735+andreasgoodstein@users.noreply.github.com>
2021-12-18 22:12:49 +0100

    solve day 16 part 1

--- runner.ts
+++ runner.ts
@@ -1,11 +1,11 @@
 import { readFileLineText } from "./parser.ts";
 import { logExecutionTime } from "./timer.ts";
 
-import { part1 } from "./day15/part1.ts";
-import { part2 } from "./day15/part2.ts";
+import { part1 } from "./day16/part1.ts";
+// import { part2 } from "./day16/part2.ts";
 
-console.log("\nday 15 part 1");
-logExecutionTime(readFileLineText("./day15/puzzle.txt"), part1);
+console.log("\nday 16 part 1");
+logExecutionTime(readFileLineText("./day16/puzzle.txt"), part1);
 
-console.log("\nday 15 part 2");
-logExecutionTime(readFileLineText("./day15/puzzle.txt"), part2);
+// console.log("\nday 16 part 2");
+// logExecutionTime(readFileLineText("./day16/puzzle.txt"), part2);
